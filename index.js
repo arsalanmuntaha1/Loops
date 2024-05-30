5c181b05433384627790d18ab233019fe5e3170e
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-05-30 10:03:17 +0500

    for loop done

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+//------------------------------------------------------> LOOPS <-------------------------------------------------------------------------
+// if we want to repeat same task multiple time we use loop
+// FOR LOOp
+let a = 0;
+for (let count = 1; count < 10; count++) {
+    console.log('Hello World');
+}
