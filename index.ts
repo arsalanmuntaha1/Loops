51a07d189fc8215424c3e1479e864fc30664dec5
arsalanmuntaha1 <arsalanmuntaha1@gmail.com>
2024-05-30 10:06:26 +0500

    update

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 // if we want to repeat same task multiple time we use loop
 
 // FOR LOOp
-let a = 0
-for(let count = 1; count < 10; count++){
-    console.log('Hello World')
+let a = 0;
+for (let count = 1; count < 10; count++) {
+  console.log("Hello World");
 }
